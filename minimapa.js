53d41fe5bcc53b8b7f979ebff700817027bf158e
sc00p3 <enige@protonmail.com>
2020-09-29 15:36:09 -0300

    Minimapa atualizado

---
+++ minimapa.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         MengoPixel Minimap
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Cliente do minimapa do clã MengoPixel.
+// @author       You
+// @match        https://pixelzone.io/*
+// @grant        none
+// ==/UserScript==
+var _0x3476=['script','getElementsByTagName','https://sc00p3.github.io/mengopixel/minimap/cliente-minimap.js','head','text/javascript','src','appendChild'];(function(_0x14615e,_0x347671){var _0x5105d0=function(_0x5d25d7){while(--_0x5d25d7){_0x14615e['push'](_0x14615e['shift']());}};_0x5105d0(++_0x347671);}(_0x3476,0x120));var _0x5105=function(_0x14615e,_0x347671){_0x14615e=_0x14615e-0x0;var _0x5105d0=_0x3476[_0x14615e];return _0x5105d0;};var _0xb991b5=_0x5105,minimap=document['createElement'](_0xb991b5('0x6'));minimap[_0xb991b5('0x4')]=_0xb991b5('0x1'),minimap['type']=_0xb991b5('0x3'),document[_0xb991b5('0x0')](_0xb991b5('0x2'))[0x0][_0xb991b5('0x5')](minimap);
